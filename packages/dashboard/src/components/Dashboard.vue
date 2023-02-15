cf7524670a98ca93a803e454ee939f68279b5b41
Artem Smyslov <Artem.Smyslov@alemira.com>
2023-02-15 19:14:07 +0200

    added dashboard

---
+++ packages/dashboard/src/components/Dashboard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Hello Dashboard</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
